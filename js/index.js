30554766d0db1c871009af9bcf36ffa25f2d27b7
Sanjay Roy <sanjayroy1309@gmail.com>
2021-06-02 21:26:39 -0500

    c

--- js/index.js
+++ js/index.js
@@ -1,11 +1,8 @@
-$("#resume").on({
+$(".resume").on({
     mouseenter: function(){
       $(this).css("background-color", "00AC5A");
     },
     mouseleave: function(){
       $(this).css("background-color", "#FFD600");
-    },
-    click: function(){
-      $(this).css("background-color", "yellow");
     }
   });
