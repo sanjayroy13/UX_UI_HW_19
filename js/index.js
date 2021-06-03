8464417cdc51e7793a7169068b53b178a833c6b7
Sanjay Roy <sanjayroy1309@gmail.com>
2021-06-02 21:19:38 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
-$("p").click(function(){
-    $(this).hide();
+$("#resume").on({
+    mouseenter: function(){
+      $(this).css("background-color", "00AC5A");
+    },
+    mouseleave: function(){
+      $(this).css("background-color", "#FFD600");
+    },
+    click: function(){
+      $(this).css("background-color", "yellow");
+    }
   });
