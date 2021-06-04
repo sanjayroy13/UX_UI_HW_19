76d586a550e6cc84bdab9442ac026f1bddaac8c0
Sanjay Roy <sanjayroy1309@gmail.com>
2021-06-04 14:44:47 -0500

    19

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
-$(".download").on({
-    mouseenter: function(){
-      $(this).css("background-color", "00AC5A");
-    },
-    mouseleave: function(){
-      $(this).css("background-color", "#FFD600");
+
+window.addEventListener("load", function(){
+    loader = document.querySelector(".loader");
+    loader.className += " hidden";
+});
+$("*").each( function () {
+    var $this = $(this);
+    if (parseInt($this.css("fontSize")) < 18) {
+        $this.css({ "font-size": "18px" });   
     }
-  });
+});
